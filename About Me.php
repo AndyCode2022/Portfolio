608da0405b4aa868dd9459a1753231321d4c67f1
Ansidion <andrew.webster403@hotmail.co.uk>
2022-12-08 17:28:01 +0000

    17:27

--- About Me.php
+++ About Me.php
@@ -1,9 +1,7 @@
 <?php require 'header.php'; ?>
 
-   <h1 class="aboutMe">About Me</h1>
-
    <article>
-      <h1 class="aboutMe">My Story</h1>
+      <h1 class="story">My Story</h1>
       <p>Hello my name is Andrew. I come from Scotland and have a passion for playing video games. I hope to become a
          website
          developer<br> or software developer one day. In the past I have built 2 computers and still love to tinker with
--- skills.php
+++ skills.php
@@ -17,12 +17,12 @@
          <img src="portfolio_images/javascript.png" alt="Javascript image">
          <h3>HTML5</h3>
          <img src="portfolio_images/HTML.png" alt="HTML image">
-         <div class="col-sm-4">
-            <h3>CSS3</h3>
-            <img src="portfolio_images/css3.png" alt="CSS3 image">
-            <h3>MySQL</h3>
-            <img src="portfolio_images/mysql.png" alt="mySQL image">
-         </div>
+      </div>
+      <div class="col-sm-4">
+         <h3>CSS3</h3>
+         <img src="portfolio_images/css3.png" alt="CSS3 image">
+         <h3>MySQL</h3>
+         <img src="portfolio_images/mysql.png" alt="mySQL image">
       </div>
    </div>
 
