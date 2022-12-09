c0c8d3699a269b6baed4e8cf96899f0baca3ad69
Ansidion <andrew.webster403@hotmail.co.uk>
2022-12-09 19:25:20 +0000

    new paragraph on about page

--- About Me.php
+++ About Me.php
@@ -7,6 +7,14 @@
       developer<br> or software developer one day. In the past I have built 2 computers and still love to tinker with
       computers.<br>
    </p>
+
+   <p>I have previously worked as a loomer which is a sort of
+      machine setter.<br>When I was in this role I perform loomings which
+      involved 2 people stripping down a Loom machine and then inserting
+      a new job into the loom machine.<br> Sometimes we would do other duties
+      as well like putting in jobs to the back of looms that didn't need
+      loomings and setting up Jacquard looms
+   </p>
 </article>
 
 <?php require 'footer.php' ?>
