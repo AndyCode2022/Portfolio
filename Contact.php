340e41b7cf2da367c90f1b22d5d6037f389e8545
Ansidion <andrew.webster403@hotmail.co.uk>
2022-12-11 17:40:05 +0000

    contact page changes

--- Contact.php
+++ Contact.php
@@ -2,7 +2,7 @@
 
 <address>
   <p>Click the link to get to my linkedIn account:
-    <a href="www.linkedin.com/in/andrew403" target="_blank">Click me</a>
+    <a href="https://www.linkedin.com/in/andrew403/" target="_blank">Click me</a>
   </p>
   If you see any bugs, please contact me at
   "andrew.webster403@hotmail.co.uk"<br>
