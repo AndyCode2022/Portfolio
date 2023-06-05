563542718d09880c466b1160c3cdd8064f10f27c
Ansidion <14023216@uhi.ac.uk>
2023-06-05 13:13:47 +0100

    changes

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
     <!-- end of react plugins -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
 </head>
 
 <body>
